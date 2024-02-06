5fd0f7b3a2b6f1a4dbf5b3bc519a11264e329340
Rafaellleao <146947546+Rafaellleao@users.noreply.github.com>
2024-02-05 22:54:03 -0300

    analise de dados e relatorio por lojas
    Projeto de uma empresa na qual trabalho e todo final de mes tenho que fazer relatorio de cada loja 
    e analisar com python o faturamento de cada uma e no word com from docx import Document 
    gerar um documento e converter em Pdf no final para imprimir .

---
+++ calendario.py
@@ -0,0 +1,58 @@
+'''calendario traduzido do ingles para portugues'''
+# calendario traduzido do ingles para portugues
+
+
+Calendario_iforp = {
+             'January': 'Janeiro',
+              'February': 'Fevereiro',
+              'March': 'Marco',
+              'April': 'Abril',
+              'May': 'Maio',
+              'June': 'junho',
+              'July': 'Julho',
+              'August': 'Agosto',
+              'september': 'Setembro',
+              'October': 'Outubro',
+              'November': 'Novembro',
+              'December': 'Dezembro'    
+               }
+
+# dicionario criado para suprir os codigos de Todas_lojas.ipynb
+dicionario_concecionaria = {
+             'grupo via sul piedade': 1000,
+             'grupo via euro olinda': 1014,
+             'jjk Autos': 1001,
+             'Rivaldo_moto': 1002,
+             'Pedragon rui b': 1003,
+             'Pedragon usados': 1017,
+             'Kia imb': 1004,
+             'granvia imb.': 1005,
+             'tambai av recife': 1006,
+             'tambai imbiribeira': 1007,
+             'honda torre novos': 1008,
+             'honda usados ': 1009,
+             'grupo via sul repasse': 1010,
+             'honda imb novos': 1011,
+             'honda prado novos': 1012,
+             'honda piedade novos': 1013,
+             'honda caruaru novos': 1015,
+             'Rivaldo_carro': 1016,
+             'hyundai': 'yyyy',
+             'hyundai_segundo': 'xxxx',
+             'via sul arruda': 1018,
+             'Kinto': 1019
+            }
+
+# dicionario criado para suprir os codigos de Recibos_caoa.ipynb
+dicionario_lojas_caoa_primeiro = {
+              'hyundai caoa': 1633,
+              'Chery casa forte': 5600,
+              'Chery imb.': 2414
+             }
+
+dicionario_lojas_caoa_segundo = {
+              'hyundai caoa_segundo': 1633,
+              'Chery casa forte_segundo': 5600,
+              'Chery imb._segundo': 2414
+             }
+
